df11831f036c171dbbf1e29142eedde3a9938f07
Joanne Cheng <joanne@thoughtbot.com>
2014-02-14 15:37:50 -0700

    pull out pearson coefficient code to seperate file

---
+++ chapter_2/pearson_coefficient.rb
@@ -0,0 +1,53 @@
+class PearsonCoefficientCalculator
+  def initialize(shared_movies, critic1_movies, critic2_movies)
+    @shared_movies = shared_movies
+    @critic1_movies = critic1_movies
+    @critic2_movies = critic2_movies
+  end
+
+  def calculate_coefficient
+    return 0 if denominator == 0
+    numerator/denominator
+  end
+
+  private
+
+  def numerator
+    sum_critic1, sum_critic2 = sum_of_preferences_by_critic
+    sum_of_ratings_products - (sum_critic1*sum_critic2/shared_movies.length)
+  end
+
+  def denominator
+    sum_critic1, sum_critic2 = sum_of_preferences_by_critic
+    sum_squares_critic1, sum_squares_critic2 = sum_of_squares_by_critic
+    @denominator ||= Math.sqrt(
+      (sum_squares_critic1-sum_critic1**2/shared_movies.length) *
+      (sum_squares_critic2-sum_critic2**2/shared_movies.length)
+    )
+  end
+
+  def sum_of_preferences_by_critic
+    [critic1_movies, critic2_movies].map do |critic|
+      shared_movies.inject(0) do |sum, movie|
+        sum + critic[movie]
+      end
+    end
+  end
+
+  def sum_of_squares_by_critic
+    [critic1_movies, critic2_movies].map do |critic|
+      shared_movies.inject(0) do |sum, movie|
+        sum + critic[movie]**2
+      end
+    end
+  end
+
+  def sum_of_ratings_products
+    shared_movies.inject(0) do |sum, movie|
+      sum + (critic1_movies[movie] * critic2_movies[movie])
+    end
+  end
+
+  attr_reader :shared_movies, :critic1_movies, :critic2_movies
+end
+
--- chapter_2/recommendations.rb
+++ chapter_2/recommendations.rb
@@ -1,5 +1,9 @@
+#
 # Chapter 2, making recommendations
 #
+
+require_relative 'pearson_coefficient'
+
 class CriticComparison
   def initialize(critic1_movies, critic2_movies)
     @critic1_movies = critic1_movies
@@ -15,20 +19,12 @@ class CriticComparison
 
   def pearson_coefficient
     return 0 if shared_movies.empty?
-    number_comparisons = shared_movies.length
 
-    sum_critic1, sum_critic2 = sum_of_preferences_by_critic
-
-    sum_squares_critic1, sum_squares_critic2 = sum_of_squares_by_critic
-
-    numerator = sum_of_products - (sum_critic1*sum_critic2/number_comparisons)
-    denominator = Math.sqrt(
-      (sum_squares_critic1-sum_critic1**2/number_comparisons) *
-      (sum_squares_critic2-sum_critic2**2/number_comparisons)
-    )
-    return 0 if denominator == 0
-
-    numerator/denominator
+    PearsonCoefficientCalculator.new(
+      shared_movies,
+      critic1_movies,
+      critic2_movies
+    ).calculate_coefficient
   end
 
   private
@@ -43,27 +39,4 @@ class CriticComparison
       sum + (critic1_movies[movie] - critic2_movies[movie])**2
     end
   end
-
-  def sum_of_preferences_by_critic
-    [critic1_movies, critic2_movies].map do |critic|
-      shared_movies.inject(0) do |sum, movie|
-        sum + critic[movie]
-      end
-    end
-  end
-
-  def sum_of_squares_by_critic
-    [critic1_movies, critic2_movies].map do |critic|
-      shared_movies.inject(0) do |sum, movie|
-        sum + critic[movie]**2
-      end
-    end
-  end
-
-  def sum_of_products
-    shared_movies.inject(0) do |sum, movie|
-      sum + (critic1_movies[movie] * critic2_movies[movie])
-    end
-  end
-
 end
